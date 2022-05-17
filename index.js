953e047e7bff4422b4853e6f5fd994dd7f3f3f5f
anisul-islam <anisul2010s@yahoo.co.uk>
2022-05-17 21:13:34 +0300

    reducer

---
+++ index.js
@@ -0,0 +1,37 @@
+// defining constants
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const ADD_USER = "ADD_USER";
+
+// state
+const initialCounterState = {
+  count: 0,
+};
+const initialUsersState = {
+  users: [{ name: "anisul islam" }],
+};
+
+// action - Object- type, payload
+const incrementCounter = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+const decrementCounter = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const addUser = () => {
+  return {
+    type: ADD_USER,
+    payload: { name: "shakil" },
+  };
+};
+
+// 1. state
+// 2. dispatch action
+// 3. reducer
+// 4. store
